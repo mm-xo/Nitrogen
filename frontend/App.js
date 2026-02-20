78ccf4900284dc9c3c4d3cfa4bb5a04ad2b34bb8
sukhdeep2004 <sukh.d0609@gmail.com>
2026-02-20 16:27:24 -0600

    updatee dev branch

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,23 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppNavigator } from "./src/navigation/AppNavigator";
-
+import { StyleSheet, View } from 'react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ExpoLocation />
-      <StatusBar style="auto" />
-    </View>
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <LocationProvider>
+      <View style={styles.container}>
+        <ExpoLocation />
+        <StatusBar style="auto" />
+      </View>
+    </LocationProvider>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: SPACING.md,
+  },
+})
